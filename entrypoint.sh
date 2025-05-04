d70bc5aa6c0ebc4b16246fd661e99f56841e923a
Sho DOUHASHI <douhashi@gmail.com>
2025-05-04 23:43:07 +0900

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@
 python3 -m venv /app/ComfyUI/venv
 . /app/ComfyUI/venv/bin/activate
 
+# Install pytorch and xformers (CUDA 12.1)
+pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu121
+pip install xformers==0.0.22 --index-url https://download.pytorch.org/whl/cu121
+
 # Install main dependencies
 pip install -r /app/ComfyUI/requirements.txt
 
